01610fcf6d3d00fc2d71a6342000f5615be69bf9
Kumargaurav Ghadge <kumarghadge10@gmail.com>
2026-06-22 23:19:22 +0530

    Uploading Today's Advance java work in which i learned the concepts like what is the Dynamic web application,server side web application,What is java EE(Jakarta EE),Whu JDBC,Why enterprise java,Dynamic Web Project Structure,Why Servlet,Whatv is Servlet,Layers on server side,Servlet APIs,How to declare Servlet

---
+++ Adv.Java/Day1/Day1/Day1_Lab/src/main/java/pages/SecondServlet.java
@@ -0,0 +1,59 @@
+package pages;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalTime;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+/*
+	 WC at app startup time
+	 -creates empty Map
+	 -Key -> /test2 (url pattern)
+	 -Value -> pages.SecondServlet (Fully Qualified class name)
+	 -runtime annotation, processed by WC
+	 -at app start up , to create a mapping
+	 -between incoming request & servlet
+ */
+
+public class SecondServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;   
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+			System.out.println("in init "+getClass());
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("in destroy "+getClass());
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+			System.out.println("in doGet "+getClass());
+			//1. set response content-type -> from the server side app-renders views (Web site)
+			response.setContentType("text/html");
+			//2. Get PrintWriter to send the dynamic response from the servlet -> client
+			try(PrintWriter pw = response.getWriter()){
+				pw.print("<h5>Hello From Servlet @ "+LocalTime.now()+"</h5>");
+			} //JVM -> pw.close() -> response is committed | rendered | sent to client
+	}
+
+}
